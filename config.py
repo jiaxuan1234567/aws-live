9682a091e7f8dc201819d1861ff932caaef37986
jiaxuan1234567 <143778461+jiaxuan1234567@users.noreply.github.com>
2025-12-10 15:20:50 +0800

    w

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 customhost = "database-1.chauwtk5goof.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
-custompass = "Bait3273"
+custompass = "Bmit3273"
 customdb = "employee"
 custombucket = "tojiaxuan-employee"
 customregion = "us-east-1"
