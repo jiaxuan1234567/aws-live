8ab3db8770268ec0de3ecffcf9e7a44e830b469a
jiaxuan1234567 <143778461+jiaxuan1234567@users.noreply.github.com>
2025-12-10 15:30:11 +0800

    .

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-customhost = "database-1.chauwtk5goof.us-east-1.rds.amazonaws.com"
+customhost = "employee.cpoqqco0u2xk.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bmit3273"
 customdb = "employee"
